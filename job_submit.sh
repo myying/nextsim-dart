78004eb37e4ffd187a42cbca1ddd66b1f3e8ce08
Yue Ying <hardyying@gmail.com>
2021-09-09 14:19:15 +0200

    update job submit code

--- job_submit.sh
+++ job_submit.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
-n=$1
-o=$2
-shift 2
-exe=$@
+nnodes=$1
+ncpus=$2
+offset_node=$3
+shift 3
+exe_command=$@
 
-function get_nodefile {
-    job_nodelist=$1
-    nodefile=''
-    if [[ ${job_nodelist:1:1} == '[' ]]; then
-        pre=${job_nodelist:0:1}
-        for id in `echo $job_nodelist |awk -F '[][]' '{print $2}' |tr ',' ' '`; do
-            if [[ ${id:4:1} == '-' ]]; then
-                for j in `seq ${id:0:4} ${id:5:4}`; do
-                    nodefile=$nodefile' '$pre$j
-                done
-            else
-                nodefile=$nodefile' '$pre$id
-            fi
-        done
-    else
-        nodefile=$job_nodelist
-    fi
-    echo $nodefile |tr ' ' '\n'
-}
 
-get_nodefile $SLURM_JOB_NODELIST |head -n $o |tail -n 1 > nodefile
-mpirun -np $n -machinefile nodefile $exe
+###this works on Betzy
+srun -N $nnodes -n $ncpus -r $offset_node $exe_command
 
+#function get_nodefile {
+#    job_nodelist=$1
+#    nodefile=''
+#    if [[ ${job_nodelist:1:1} == '[' ]]; then
+#        pre=${job_nodelist:0:1}
+#        for id in `echo $job_nodelist |awk -F '[][]' '{print $2}' |tr ',' ' '`; do
+#            if [[ ${id:4:1} == '-' ]]; then
+#                for j in `seq ${id:0:4} ${id:5:4}`; do
+#                    nodefile=$nodefile' '$pre$j
+#                done
+#            else
+#                nodefile=$nodefile' '$pre$id
+#            fi
+#        done
+#    else
+#        nodefile=$job_nodelist
+#    fi
+#    echo $nodefile |tr ' ' '\n'
+#}
+
+#get_nodefile $SLURM_JOB_NODELIST |head -n $((offset_node+$nnodes)) |tail -n $nnodes > nodefile
+#mpirun -np $ncpus -machinefile nodefile $exe_command
 
